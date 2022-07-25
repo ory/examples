fa0eb0145e5fa1c605cf302d2431346c8ccee4f2
vinckr <vincent@ory.sh>
2022-07-25 18:25:57 +0200

    u

---
+++ src/scripts/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euxo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")/../.."
+
+export ORY_SDK_URL=https://playground.projects.oryapis.com
+export ORY_UI_URL=https://playground.projects.oryapis.com/ui
+
+#
+# Please add any build steps to the Makefile and not here!
+#
+
+cd django-ory-cloud && \
+  poetry install && \
+  cd mysite && \
+  poetry run python manage.py migrate && \
+poetry run python manage.py runserver &
+
+
+trap "exit" INT TERM ERR
+trap 'kill $(jobs -p)' EXIT
+
+npx wait-on -v -t 300000 \
+  tcp:localhost:8000 \
+
+npm run test
---
+++ tests/playwright/helpers.ts
@@ -0,0 +1,4 @@
+export const randomString = () =>
+  Math.random().toString(36).substring(2, 15) +
+  Math.random().toString(36).substring(2, 15)
+export const randomEmail = () => randomString() + "@" + randomString() + ".dev"
