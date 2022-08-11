2184a012ffe73cc260ee4139fa7a40910e7590c1
Vincent Kraus <mail@vinckr.com>
2022-08-12 00:28:50 +0200

    u

--- tests/playwright/django-ory-cloud.spec.ts
+++ tests/playwright/django-ory-cloud.spec.ts
@@ -1,5 +1,5 @@
-import { test, expect, Page } from "@playwright/test"
-import { randomEmail, randomString } from "./helpers"
+import { test, expect, Page } from '@playwright/test'
+import { randomEmail, randomString } from './helpers'
 
 const login = async (page: Page) => {
   const email = randomEmail()
@@ -9,13 +9,13 @@ const login = async (page: Page) => {
   return email
 }
 
-test.describe("django-ory-cloud", () => {
-  for (const app of [
-    { url: "http://localhost:4000/", name: "django" },
-  ]) {
-    test.describe("django", async () => {
-      test("able to use login and sign up", async ({ page }) => {
-        await page.goto(app.url, { waitUntil: "networkidle" })
+test.describe('django-ory-cloud', () => {
+  const url = 'http://localhost:4000/'
+  const name = 'django'
+  {
+    test.describe('django', async () => {
+      test('login and sign up', async ({ page }) => {
+        await page.goto(app.url, { waitUntil: 'networkidle' })
 
         await page.locator('text=Sign in').click()
         await expect(page).toHaveURL(/.*\/ui\/login.*/)
@@ -25,7 +25,7 @@ test.describe("django-ory-cloud", () => {
 
         const email = await login(page)
         await expect(page).toHaveURL(/.*\/ui\/welcome.*/)
-        await expect(page.locator("body")).toContainText(email)
+        await expect(page.locator('body')).toContainText(email)
       })
     })
   }
