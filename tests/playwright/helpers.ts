f3fbd0ca0019412fe045bc04e4b4b44c3f2348db
Vincent Kraus <mail@vinckr.com>
2022-08-12 10:05:34 +0200

    chore: removed unused test

--- tests/playwright/helpers.ts
+++
@@ -1,4 +0,0 @@
-export const randomString = () =>
-  Math.random().toString(36).substring(2, 15) +
-  Math.random().toString(36).substring(2, 15)
-export const randomEmail = () => randomString() + "@" + randomString() + ".dev"
