d8bca0e835309cf5b5fe3647cafca7bff3e37972
Vincent <vincent@ory.sh>
2022-08-18 01:14:34 +0200

    feat: add tests to django example (#42)
    * u
    
    * fix: add makefile
    
    * fix: add ory proxy
    
    * fix: django test
    
    * chore: apply suggestions from code review
    
    Co-authored-by: Kevin Goslar <kevin.goslar@gmail.com>
    
    * fix: install poetry
    
    * fix: install python
    
    * fix: makefile
    
    * u
    
    * u
    
    * u
    
    * u
    
    * u
    
    * u
    
    * u
    
    * u
    
    * u
    
    * u
    
    * u
    
    * test: address pull request suggestions (#47)
    
    * chore: update repository templates
    
    [skip ci] - updated repository templates to https://github.com/ory/meta/commit/32137ce422e5241bef5d7fd12c928ad39e34add0
    
    * chore: update repository templates
    
    [skip ci] - updated repository templates to https://github.com/ory/meta/commit/430183216bf2d03ee7e20d26e19000c2d55fbb54
    
    * Move files into django-ory-cloud dir
    
    * package-lock.json package.json
    
    * .gitignore package-lock.json package.json package-lock.json package.json
    
    Co-authored-by: aeneasr <3372410+aeneasr@users.noreply.github.com>
    
    * chore: removed unused test
    
    * chore: run django test in folder
    
    * chore: run django test in folder
    
    * chore: fix makefile
    
    * fix: rename workflow
    
    * fix: remove test artifacts
    
    * fix: makefile
    
    * chore: apply suggestions from code review
    
    * chore: django-ory-cloud/tests/playwright/django-ory-cloud.spec.ts
    
    Co-authored-by: Kevin Goslar <kevin.goslar@gmail.com>
    
    * u
    
    * u
    
    * u
    
    * chore: clean Makefile
    
    * fix: test out working dir only on test
    
    * chore: delete unused config
    
    * chore: shorten workflow name
    
    * u
    
    * chore: clean Makefile
    
    * chore: clean script
    
    Co-authored-by: Kevin Goslar <kevin.goslar@gmail.com>
    Co-authored-by: aeneasr <3372410+aeneasr@users.noreply.github.com>

---
+++ django-ory-cloud/tests/playwright/django-ory-cloud.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect, Page } from '@playwright/test'
+import { randomEmail, randomString } from './helpers'
+
+const login = async (page: Page) => {
+  const email = randomEmail()
+  await page.fill('[name="traits.email"]', email)
+  await page.fill('[name="password"]', randomString())
+  await page.click('[value="password"]')
+  return email
+}
+
+test.describe('django-ory-cloud', () => {
+  const url = 'http://localhost:4000/'
+  const name = 'django'
+  test('login and sign up', async ({ page }) => {
+    await page.goto(url, { waitUntil: 'networkidle' })
+    await page.locator('text=Sign in').click()
+    await expect(page).toHaveURL(/.*\/ui\/login.*/)
+    await page.click('[data-testid="cta-link"]')
+    await expect(page).toHaveURL(/.*\/ui\/registration.*/)
+    const email = await login(page)
+    await expect(page).toHaveURL(/.*\/ui\/welcome.*/)
+    await expect(page.locator('body')).toContainText(email)
+  })
+})
