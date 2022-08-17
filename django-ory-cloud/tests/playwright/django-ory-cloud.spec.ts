3ad5fee392458cb8f2b623078bfb7c0f370c4792
Vincent Kraus <mail@vinckr.com>
2022-08-17 21:17:11 +0200

    chore: apply suggestions from code review

--- django-ory-cloud/tests/playwright/django-ory-cloud.spec.ts
+++ django-ory-cloud/tests/playwright/django-ory-cloud.spec.ts
@@ -16,13 +16,10 @@ test.describe('django-ory-cloud', () => {
     test.describe('django', async () => {
       test('login and sign up', async ({ page }) => {
         await page.goto(url, { waitUntil: 'networkidle' })
-
         await page.locator('text=Sign in').click()
         await expect(page).toHaveURL(/.*\/ui\/login.*/)
-
         await page.click('[data-testid="cta-link"]')
         await expect(page).toHaveURL(/.*\/ui\/registration.*/)
-
         const email = await login(page)
         await expect(page).toHaveURL(/.*\/ui\/welcome.*/)
         await expect(page.locator('body')).toContainText(email)
