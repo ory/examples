54eaea4cf3f33d50249064db3ca555e9657dc245
Vincent Kraus <mail@vinckr.com>
2022-08-18 00:59:55 +0200

    u

--- django-ory-cloud/playwright.config.ts
+++ django-ory-cloud/playwright.config.ts
@@ -28,7 +28,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* the number of workers, undefined for parallel tests */
-  workers: 1,
+  workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
