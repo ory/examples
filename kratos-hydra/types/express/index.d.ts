dc91a18319906c7a304790d97020e6c13553ccaf
Matt Barnicle <mattbarnicle@users.noreply.github.com>
2023-02-21 11:14:57 +0100

    feat: hydra & kratos integration example (#2976) (#68)
    * feat: hydra & kratos integration example
    
    * * fix: removed files from kratos-hydra that were incorrectly copied
    
    * style: code updates on kratos-hydra from running "make format"

---
+++ kratos-hydra/types/express/index.d.ts
@@ -0,0 +1,10 @@
+// Copyright © 2023 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
+import { Session } from "@ory/client"
+
+declare module "express" {
+  export interface Request {
+    session?: Session
+  }
+}
