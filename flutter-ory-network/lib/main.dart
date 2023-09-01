e7658a3ef4c6507fc0c3acd387ce34b164ae0987
Alexandra Talalaieva <25621530+sashatalalasha@users.noreply.github.com>
2023-09-01 11:32:42 +0200

    fix: change package and folder name

--- flutter-ory-network/lib/main.dart
+++ flutter-ory-network/lib/main.dart
@@ -48,45 +48,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyAppView(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Flutter Ory Network Demo',
+      home: MyAppView(),
     );
   }
 }
 
 class MyAppView extends StatefulWidget {
-  const MyAppView({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyAppView({super.key});
 
   @override
   State<MyAppView> createState() => _MyAppViewState();
