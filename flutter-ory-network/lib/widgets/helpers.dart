c492c482cdfa9f23ead6fc424966d96441c40822
Alexandra Talalaieva <25621530+sashatalalasha@users.noreply.github.com>
2023-11-13 13:49:12 +0100

    chore: add node helper & show code nodes

--- flutter-ory-network/lib/widgets/helpers.dart
+++ flutter-ory-network/lib/widgets/helpers.dart
@@ -1,7 +1,10 @@
 // Copyright © 2023 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
 
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
+import 'package:built_collection/built_collection.dart';
 import 'package:flutter/material.dart';
 import 'package:ory_client/ory_client.dart';
 
@@ -89,4 +92,31 @@ buildInputNode<T extends Bloc>(
     default:
       return InputNode<T>(node: node, onChange: onInputChange);
   }
+}
+
+List<UiNode> getNodesOfGroup(UiNodeGroupEnum group, BuiltList<UiNode> nodes) {
+  return nodes.where((node) {
+    if (node.group == group) {
+      if (group == UiNodeGroupEnum.oidc) {
+        if (node.attributes.oneOf.isType(UiNodeInputAttributes)) {
+          final attributes =
+              node.attributes.oneOf.value as UiNodeInputAttributes;
+          return Platform.isAndroid
+              ? !attributes.value!.asString.contains('ios')
+              : attributes.value!.asString.contains('ios');
+        }
+      } else {
+        if (node.attributes.oneOf.isType(UiNodeInputAttributes)) {
+          final attributes =
+              node.attributes.oneOf.value as UiNodeInputAttributes;
+          if (attributes.type == UiNodeInputAttributesTypeEnum.hidden) {
+            return false;
+          } else {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }).toList();
 }
